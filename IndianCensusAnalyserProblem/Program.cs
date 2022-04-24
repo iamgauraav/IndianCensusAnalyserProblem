d13c2de7a758d39d461a92ef67ea888385e0efb7
iamgauraav <iamgauraav@gmail.com>
2022-04-24 23:29:03 +0530

    Added welcome message to master branch

---
+++ IndianCensusAnalyserProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianCensusAnalyserProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to IndianCensus Analyser Problem on master branch");
+        }
+    }
+}
